d53104f6d69dd3a8750576c699a1183e2e46841a
Robert Stenson <rob.stenson@gmail.com>
2012-04-17 15:15:03 -0700

    with chords

--- functions.js
+++ functions.js
@@ -35,9 +35,42 @@ var attachBuffersToConvolvers = function(env) {
     env.convolvers[name].buffer = env.buffers[name];
     env.convolvers[name].connect(env.context.destination);
   });
-
-  console.log(env.convolvers);
 };
+
+var closestFromList = function(list, x) {
+  var closest = list[0];
+  var shortest = Math.abs(x - closest);
+
+  for (var i = 1; i < list.length; i++) {
+    var n = list[i];
+    var interval = Math.abs(x - n);
+
+    if (interval === 0) {
+      return x;
+    } else if(interval < shortest) {
+      closest = n;
+      shortest = interval;
+    } else {
+      return closest;
+    }
+  }
+
+  return closest;
+};
+
+var autoTune = (function() {
+  var a = Math.pow(2, 1/12);
+
+  return function(env, f) {
+    var n = Math.round(Math.log(f / 1) / Math.log(a));
+    var adjust = Math.floor(n/12);
+    var m = n % 12;
+    var closest = closestFromList(env.chords[env.currentChord], m);
+
+    console.log(f, n, closest, closest+(adjust*12));
+    return Math.pow(a, (closest + (adjust*12)));
+  };
+})();
 
 var playSample = function(env, hash) {
   /* .rate, .volume, .buffer, .convolver, .pan */
@@ -48,7 +81,11 @@ var playSample = function(env, hash) {
   var dryGain = context.createGainNode();
   var wetGain = context.createGainNode();
 
-  source.playbackRate.value = Math.round(hash.rate || 1);
+  //console.log(hash.rate);
+  //var rate = autoTune(env, hash.rate || 1);
+  //autoTune(env, hash.rate);
+  //source.playbackRate.value = (hash.rate || 1);
+  source.playbackRate.value = autoTune(env, hash.rate || 1);
   wetGain.gain.value = hash.volume || 1;
   dryGain.gain.value = (hash.volume || 1) / 4;
   source.buffer = env.buffers[hash.buffer];
@@ -62,8 +99,9 @@ var playSample = function(env, hash) {
 
   source.noteOn(hash.start || 0);
 
-  console.log(source.playbackRate.value);
+  //console.log(source.playbackRate.value);
   hash.source = source;
+  hash.ran = context.currentTime;
   return hash;
 };
 
@@ -97,25 +135,23 @@ var buildSignal = function(env, frequency) {
 };
 
 var notifyRequestStart = function(env, details) {
-  //if (details.type !== "stylesheet") return;
-
   //console.log("start", details);
-  env.howMany++;
   //env.requests[details.requestId] = buildSignal(env, 110 + (env.howMany*100));
+
+  env.howMany++;
+
   var pan = Math.random()*10 - 5;
 
   env.requests[details.requestId] = playSample(env, {
     buffer: "start",
     convolver: "kitchen",
-    volume: 0.45,
+    volume: 0.75,
     pan: (pan > 0) ? (pan + 5) : (pan - 5),
     rate: Math.random()*5 + 3
   });
 };
 
 var notifyRequestStop = function(env, details) {
-  //if (details.type !== "stylesheet") return;
-
   //console.log("stop", details);
   env.howMany--;
   //env.requests[details.requestId].node.disconnect();
@@ -124,28 +160,29 @@ var notifyRequestStop = function(env, details) {
   var requestSample = env.requests[details.requestId];
   if (!requestSample) return;
 
-  details.responseHeaders.forEach(function(header) {
-    if (header.name === "Content-Length") contentLength = header.value;
-  });
+  var runtime = env.context.currentTime - requestSample.ran;
 
-  console.log(contentLength);
+  details.responseHeaders.forEach(function(header) {
+    var name = header.name.toLowerCase();
+    if (name === "content-length") contentLength = header.value;
+  });
 
   if (details.statusCode > 400) {
     playSample(env, {
       buffer: "trumpet",
-      pan: 0.0,
       convolver: "spring",
+      pan: 0.0,
       rate: 1.9
     });
   } else {
     var buffer = env.typesToBuffers[details.type] || "fetch";
 
     playSample(env, {
-      //buffer: details.fromCache ? "cache" : "fetch",
       buffer: buffer,
       convolver: details.fromCache ? "telephone" : "spring",
-      volume: 1.5, //details.fromCache ? 0.15 : 0.45,
+      volume: 0.95,
       pan: requestSample.pan || Math.random()*10 - 5,
+      //pan: (runtime - 0.25) * 20 + (Math.random()*3-1.5),
       rate: 20000 / (contentLength * 2)
     });
   }
--- popup.js
+++ popup.js
@@ -6,12 +6,27 @@ var env = {
   howMany: 0,
   convolverNames: ["kitchen", "telephone", "spring"],
   typesToBuffers: {
-             image: "start",
-        stylesheet: "cache",
-            script: "note",
-    xmlhttprequest: "cache"
-  }
+             image: "cache",
+        stylesheet: "note",
+            script: "fetch",
+    xmlhttprequest: "ukelele"
+  },
+  runtimes: [],
+  chords: [
+    [1, 3, 6, 7],
+    [0, 2, 5, 9, 11],
+    [2, 5, 7, 9]
+  ],
+  currentChord: 0
 };
+
+setInterval(function() {
+  if (env.currentChord == env.chords.length - 1) {
+    env.currentChord = 0;
+  } else {
+    env.currentChord = env.currentChord + 1;
+  }
+}, 5000);
 
 var sampleMap = {
               fail: "hat",
@@ -41,39 +56,69 @@ loadSamplesFromMap(env, sampleMap, "sounds/*.wav", function() {
     notifyRequestStop(env, details);
   }, permissions, ["responseHeaders"]);
 
-//   var rate = 1;
-//
-//   setInterval(function() {
-//     playSample(env, {
-//       buffer: "guitar",
-//       convolver: "spring",
-//       volume: 0.45,
-//       pan: Math.random() * 10 - 5,
-//       rate: rate
-//     });
-//
-//     rate = rate + 1;
-//   }, 300);
-//
-//   setInterval(function() {
-//     playSample(env, {
-//       buffer: "ukelele",
-//       convolver: "spring",
-//       volume: 0.35,
-//       pan: Math.random() * 10 - 5,
-//       rate: rate
-//     });
-//
-//     rate = Math.round(Math.random() * 10);
-//   }, 800);
-//
-//   setInterval(function() {
-//     playSample(env, {
-//       buffer: "note",
-//       convolver: "spring",
-//       volume: 0.35,
-//       pan: 0,
-//       rate: Math.random() > 0.5 ? 1 : 3
-//     });
-//   }, 1000);
+
+  var intervals = [0, 2, 4, 7, 9, 11];
+
+  var count = 1;
+  var a = Math.pow(2, 1/12);
+
+  var note = function(i) {
+    return Math.pow(a, i);
+  };
+
+  // setInterval(function() {
+  //
+  //   playSample(env, {
+  //     buffer: "ukelele",
+  //     convolver: "telephone",
+  //     volume: 0.45,
+  //     pan: 5.0,
+  //     rate: note(intervals[Math.floor(Math.random()*intervals.length)] + 12)
+  //   });
+  //
+  //   playSample(env, {
+  //     buffer: "guitar",
+  //     convolver: "telephone",
+  //     volume: 0.45,
+  //     pan: -10.0,
+  //     rate: note(intervals[Math.floor(Math.random()*intervals.length)] + 12)
+  //   });
+  //
+  // }, 100);
+
+  // var rate = 1;
+  //
+  // setInterval(function() {
+  //   playSample(env, {
+  //     buffer: "guitar",
+  //     convolver: "spring",
+  //     volume: 0.45,
+  //     pan: Math.random() * 10 - 5,
+  //     rate: rate
+  //   });
+  //
+  //   rate = rate + 1;
+  // }, 100);
+  //
+  // setInterval(function() {
+  //   playSample(env, {
+  //     buffer: "ukelele",
+  //     convolver: "spring",
+  //     volume: 0.35,
+  //     pan: Math.random() * 10 - 5,
+  //     rate: rate
+  //   });
+  //
+  //   rate = Math.round(Math.random() * 10);
+  // }, 500);
+  //
+  // setInterval(function() {
+  //   playSample(env, {
+  //     buffer: "note",
+  //     convolver: "spring",
+  //     volume: 0.35,
+  //     pan: 0,
+  //     rate: Math.random() > 0.5 ? 1 : 3
+  //   });
+  // }, 800);
 });
